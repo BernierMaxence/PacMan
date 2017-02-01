96060472cb1cec5c794951df1e28f58e1b95b993
Unknown <maxencebernier@hotmail.fr>
2017-02-01 15:53:43 +0100

    add Charcter and PacMan classes

---
+++ Charcter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Charcter
+{
+    private Position _position; 
+
+
+	public Charcter(Position position)
+	{
+        this._position = position; 
+
+	}
+
+    public void setPosition(Position position)
+    {
+        this._position = position; 
+    }
+    public Position getPostion()
+    {
+        return _position; 
+    }
+}
---
+++ PacMan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class PacMan : Character 
+{
+    private int _life; 
+
+	public PacMan(Position position, int life )
+	{
+        base(position);
+        this._life = life; 
+	}
+
+    public int getLife()
+    {
+        return _life; 
+    }
+    public void setLife(int life)
+    {
+        this._life = life; 
+    }
+
+    public void looseLife()
+    {
+        --life; 
+    }
+}
