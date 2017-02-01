95155b034dfd726be7759aa4ed15b16f2287a211
Unknown <maxencebernier@hotmail.fr>
2017-02-01 15:35:39 +0100

    add Position class

---
+++ Position.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Position
+{
+    private int x;
+    private int y;
+
+    public Position(int x, int y)
+	{
+        this.x = x;
+        this.y = y;        
+
+	}
+
+    public int getX()
+    {
+        return x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+}
