140eed2f37652fad248e7beccf6d4a178d745a66
Unknown <maxencebernier@hotmail.fr>
2017-02-01 16:12:58 +0100

    move files

--- PacMan/PacMan/AnimatedObject.cs
+++ PacMan/PacMan/AnimatedObject.cs
@@ -5,14 +5,15 @@ using Microsoft.Xna.Framework.Graphics;   //   for Texture2D
 using Microsoft.Xna.Framework;  //  for Vector2
 namespace PacMan
 {
-    private Texture2D _texture;    //  sprite texture 
-    private Vector2 _position;     //  sprite position on screen
-    private Vector2 _size;         //  sprite size in pixels
-
-
-
+    
     public class AnimatedObject
     {
+        private Texture2D _texture;    //  sprite texture 
+        private Vector2 _position;     //  sprite position on screen
+        private Vector2 _size;         //  sprite size in pixels
+
+
+
         public AnimatedObject(Texture2D texture, Vector2 position, Vector2 size)
         {
             this._texture = texture;
