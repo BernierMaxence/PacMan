9f19d3d53a4fc2b7db19c6de7e5327f812305341
Eriwyr <mail@v-chabrier.me>
2017-02-03 11:52:53 +0100

    gost moves

--- PacMan/PacMan/GostCharacter.cs
+++ PacMan/PacMan/GostCharacter.cs
@@ -15,8 +15,25 @@ namespace PacMan
         public Direction randomDirection()
         {
             Random rand = new Random();
-            return (Direction)rand.Next(0, 4);
-
+            Direction dir = (Direction)rand.Next(0, 4);
+            Console.WriteLine("dir " + dir);
+            return dir; 
+        }
+        public Direction nextDirection()
+        {
+            switch(this.Direction)
+            {
+                case Direction.Down:
+                    return Direction.Right;
+                case Direction.Right:
+                    return Direction.Up;
+                case Direction.Up:
+                    return Direction.Left;
+                case Direction.Left:
+                    return Direction.Down;
+                default:
+                    return Direction.Down; 
+            }
         }
     }
     
